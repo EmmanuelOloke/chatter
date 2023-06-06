14e6c43299af0ad7a977920ec9ab464c0f057c55
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2023-06-07 00:33:01 +0100

    ChakraProvider, CacheProvider, and metadata error yet to fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,25 @@
+'use client';
+
 import './globals.css';
 import { Inter } from 'next/font/google';
+import { ChakraProvider } from '@chakra-ui/react';
+import { CacheProvider } from '@chakra-ui/next-js';
 
 const inter = Inter({ subsets: ['latin'] });
 
-export const metadata = {
-  title: 'Chatter: A Haven for Text-Based Content',
-  description: 'Unleash the power of words, Connect with Like-minded Readers and Writers',
-};
+// export const metadata = {
+//   title: 'Chatter: A Haven for Text-Based Content',
+//   description: 'Unleash the power of words, Connect with Like-minded Readers and Writers',
+// };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <CacheProvider>
+          <ChakraProvider>{children}</ChakraProvider>
+        </CacheProvider>
+      </body>
     </html>
   );
 }
