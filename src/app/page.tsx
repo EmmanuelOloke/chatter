751ad74970e862eecb31b4e6f5bcd1efc0641444
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2023-06-06 16:34:43 +0100

    use nextjs recommended 'use client'

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
-import { Text } from '../lib/chakra';
+'use client';
+
+import NavBar from '@/components/NavBar';
+import { Text } from '@chakra-ui/react';
 
 export default function Home() {
   return (
     <main>
+      <NavBar />
       <Text>This is where it all starts</Text>
     </main>
   );
