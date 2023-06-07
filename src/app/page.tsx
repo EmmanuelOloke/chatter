8473e5362a246b4860c7e37f15125837b5853133
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2023-06-07 17:35:16 +0100

    render WhyJoinChatter component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@
 import { AboutChatter } from '@/components/AboutChatter';
 import { Hero } from '@/components/Hero';
 import NavBar from '@/components/NavBar';
+import { WhyJoinChatter } from '@/components/WhyJoinChatter';
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <NavBar />
       <Hero />
       <AboutChatter />
+      <WhyJoinChatter />
     </main>
   );
 }
