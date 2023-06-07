f604241f2c5390dab419802ead765898658ee976
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2023-06-07 17:34:51 +0100

    created a data file to hold why join chatter section card details

---
+++ public/data/whyJoinChatterCardsData.ts
@@ -0,0 +1,25 @@
+export const whyJoinChatterCards = [
+  {
+    id: 1,
+    cardIcon: './images/icons/analytics.svg',
+    cardIconAlt: 'analytics icon',
+    cardTitle: 'Analytics',
+    cardText:
+      'Analytics to track the number of views, likes and comment and also analyze the performance of your articles over a period of time',
+  },
+  {
+    id: 2,
+    cardIcon: './images/icons/social-interaction.svg',
+    cardIconAlt: 'social interaction icon',
+    cardTitle: 'Social Interactions',
+    cardText:
+      'Users on the platform can interact with posts they like, comment and engage in discussions',
+  },
+  {
+    id: 3,
+    cardIcon: './images/icons/content-creation.svg',
+    cardIconAlt: 'content creation icon',
+    cardTitle: 'Content Creation',
+    cardText: 'Write nice and appealing with our in-built markdown, a rich text editor',
+  },
+];
