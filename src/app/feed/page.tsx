1c14735fbaa2af8d073475ccd0b4fbca656d319b
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2023-06-15 13:31:43 +0100

    created feed route for sidebar to be rendered in

---
+++ src/app/feed/page.tsx
@@ -0,0 +1,12 @@
+import SideBar from '@/components/SideBar';
+import React from 'react';
+
+const Feed = () => {
+  return (
+    <div>
+      <SideBar />
+    </div>
+  );
+};
+
+export default Feed;
