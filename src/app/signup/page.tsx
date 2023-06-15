82c0fbeb79bd0c54672ee9e86a9c420988eca561
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2023-06-15 13:05:49 +0100

    login tab ui done

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -35,7 +35,7 @@ const Signup = () => {
 
   return (
     <Box width="100vw">
-      <HStack gap="15rem" alignItems="flex-start">
+      <HStack gap="15rem">
         <Flex
           h="100vh"
           background={`linear-gradient(rgba(0,0,0,0.55), rgba(0,0,0,0.55)), url('./assets/images/signup-image/chatter-signup-image.jpeg')`}
@@ -135,7 +135,6 @@ const Signup = () => {
 
                     <Flex flexDir="column">
                       <FormLabel>Confirm Password</FormLabel>
-                      {/* <Flex alignItems="center"> */}
                       <InputGroup size="md">
                         <Input
                           pr="4.5rem"
@@ -154,7 +153,6 @@ const Signup = () => {
                           {confirmPasswordShow ? <ViewOffIcon /> : <ViewIcon />}
                         </InputRightElement>
                       </InputGroup>
-                      {/* </Flex> */}
                     </Flex>
 
                     <Flex>
@@ -207,7 +205,52 @@ const Signup = () => {
               </TabPanel>
 
               <TabPanel>
-                <p>two!</p>
+                <Text fontSize="2rem" fontWeight="500" textAlign="center" mb={5}>
+                  Welcome back
+                </Text>
+
+                <FormControl>
+                  <Flex flexDir="column" gap="1.5rem">
+                    <Flex flexDir="column">
+                      <FormLabel>Email address</FormLabel>
+                      <Input type="email" placeholder="Johndoe@gmail.com" h="3rem" />
+                    </Flex>
+
+                    <Flex flexDir="column">
+                      <FormLabel>Password</FormLabel>
+                      <InputGroup size="md">
+                        <Input
+                          pr="4.5rem"
+                          type={passwordShow ? 'text' : 'password'}
+                          placeholder="Enter password"
+                          h="3rem"
+                        />
+                        <InputRightElement
+                          width="4.5rem"
+                          onClick={handlePasswordShow}
+                          cursor="pointer"
+                          display="flex"
+                          alignItems="center"
+                          mt="0.2rem"
+                        >
+                          {passwordShow ? <ViewOffIcon /> : <ViewIcon />}
+                        </InputRightElement>
+                      </InputGroup>
+                    </Flex>
+
+                    <Button
+                      variant="solid"
+                      backgroundColor="#543EE0"
+                      color="#FFF"
+                      size="lg"
+                      width="100%"
+                      height="3rem"
+                      _hover={{ backgroundColor: '#7a67f4' }}
+                    >
+                      Login
+                    </Button>
+                  </Flex>
+                </FormControl>
               </TabPanel>
             </TabPanels>
           </Tabs>
