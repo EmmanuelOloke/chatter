451d398598d4ffb1adcb5d31c74616a81b52ecf9
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2023-06-16 14:31:56 +0100

    overview navbar section done

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -13,64 +13,72 @@ const SideBar = () => {
   return (
     <Flex
       flexDir="column"
-      alignItems="center"
       w="13rem"
       pos="sticky"
       h="100vh"
       borderRight="1px solid #D0D0D0"
       left="0"
       pt={3}
+      gap={5}
     >
-      <Text color="#543EE0" fontSize="1.7rem">
+      <Flex color="#543EE0" fontSize="1.7rem" justifyContent="center">
         CHATTER
-      </Text>
+      </Flex>
 
-      <Text fontWeight="400" fontSize="1rem">
-        Overview
-      </Text>
+      <Flex justifyContent="center">
+        <Flex fontWeight="400" fontSize="1rem" w="7.5rem">
+          Overview
+        </Flex>
+      </Flex>
 
-      <VStack>
-        <NextLink href="/feed" passHref>
-          <Flex>
-            <Icon viewBox="0 0 22 22" color="red.500">
-              <FeedSvg color="red" />
-            </Icon>
-            <Text>Feed</Text>
-          </Flex>
-        </NextLink>
-        <NextLink href="/feed" passHref>
-          <Flex>
-            <Icon viewBox="0 0 22 22" color="red.500">
-              <BookmarkSvg color="red" />
-            </Icon>
-            <Text>Bookmarks</Text>
-          </Flex>
-        </NextLink>
-        <NextLink href="/feed" passHref>
-          <Flex>
-            <Icon viewBox="0 0 22 22" color="red.500">
-              <TeamSvg color="red" />
-            </Icon>
-            <Text>Team Blogs</Text>
-          </Flex>
-        </NextLink>
-        <NextLink href="/feed" passHref>
-          <Flex>
-            <Icon viewBox="0 0 22 22" color="red.500">
-              <DraftsSvg color="red" />
-            </Icon>
-            <Text>Drafts</Text>
-          </Flex>
-        </NextLink>
-        <NextLink href="/feed" passHref>
-          <Flex>
-            <Icon viewBox="0 0 22 22" color="red.500">
-              <AnalyticsSvg color="red" />
-            </Icon>
-            <Text>Analytics</Text>
-          </Flex>
-        </NextLink>
-      </VStack>
+      <Flex justifyContent="flex-end">
+        <VStack w="9.5rem" alignItems="left">
+          <NextLink href="/feed" passHref>
+            <Flex justifyContent="left" alignItems="center" gap={3}>
+              <Icon viewBox="0 0 22 22" color="red.500">
+                <FeedSvg color="red" />
+              </Icon>
+              <Text>Feed</Text>
+            </Flex>
+          </NextLink>
+
+          <NextLink href="/feed" passHref>
+            <Flex justifyContent="left" alignItems="center" gap={3}>
+              <Icon viewBox="0 0 22 22" color="red.500">
+                <BookmarkSvg color="red" />
+              </Icon>
+              <Text>Bookmarks</Text>
+            </Flex>
+          </NextLink>
+
+          <NextLink href="/feed" passHref>
+            <Flex justifyContent="left" alignItems="center" gap={3}>
+              <Icon viewBox="0 0 22 22" color="red.500">
+                <TeamSvg color="red" />
+              </Icon>
+              <Text>Team Blogs</Text>
+            </Flex>
+          </NextLink>
+
+          <NextLink href="/feed" passHref>
+            <Flex justifyContent="left" alignItems="center" gap={3}>
+              <Icon viewBox="0 0 22 22" color="red.500">
+                <DraftsSvg color="red" />
+              </Icon>
+              <Text>Drafts</Text>
+            </Flex>
+          </NextLink>
+
+          <NextLink href="/feed" passHref>
+            <Flex justifyContent="left" alignItems="center" gap={3}>
+              <Icon viewBox="0 0 22 22" color="red.500">
+                <AnalyticsSvg color="red" />
+              </Icon>
+              <Text>Analytics</Text>
+            </Flex>
+          </NextLink>
+        </VStack>
+      </Flex>
     </Flex>
   );
 };
