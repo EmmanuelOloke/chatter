a89c569c9b39424c6053a5a71c0647ed21ed2487
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2023-06-17 19:34:22 +0100

    render lengthy content for testing

--- src/app/feed/page.tsx
+++ src/app/feed/page.tsx
@@ -1,10 +1,57 @@
+'use client';
+
 import SideBar from '@/components/SideBar';
+import { Box, Flex, HStack, Text, VStack } from '@chakra-ui/react';
 import React from 'react';
 
 const Feed = () => {
   return (
     <div>
-      <SideBar />
+      <HStack alignItems="flex-start">
+        <Flex pos="sticky" top={0}>
+          <SideBar />
+        </Flex>
+        <VStack>
+          <Text>Feed navbar goes here</Text>
+          <Text>Feed navbar goes here</Text>
+          <Text>Feed navbar goes here</Text>
+          <Text>Feed navbar goes here</Text>
+          <Text>Feed navbar goes here</Text>
+          <Text>Feed navbar goes here</Text>
+          <Text>Feed navbar goes here</Text>
+          <Text>Feed navbar goes here</Text>
+          <Text>Feed navbar goes here</Text>
+          <Text>Feed navbar goes here</Text>
+          <Text>Feed navbar goes here</Text>
+          <Text>Feed navbar goes here</Text>
+          <Text>Feed navbar goes here</Text>
+          <Text>Feed navbar goes here</Text>
+          <Text>Feed navbar goes here</Text>
+          <Text>Feed navbar goes here</Text>
+          <Text>Feed navbar goes here</Text>
+          <Text>Feed navbar goes here</Text>
+          <Text>Feed navbar goes here</Text>
+          <Text>Feed navbar goes here</Text>
+          <Text>Feed navbar goes here</Text>
+          <Text>Feed navbar goes here</Text>
+          <Text>Feed navbar goes here</Text>
+          <Text>Feed navbar goes here</Text>
+          <Text>Feed navbar goes here</Text>
+          <Text>Feed navbar goes here</Text>
+          <Text>Feed navbar goes here</Text>
+          <Text>Feed navbar goes here</Text>
+          <Text>Feed navbar goes here</Text>
+          <Text>Feed navbar goes here</Text>
+          <Text>Feed navbar goes here</Text>
+          <Text>Feed navbar goes here</Text>
+          <Text>Feed navbar goes here</Text>
+          <Text>Feed navbar goes here</Text>
+          <Text>Feed navbar goes here</Text>
+          <Text>Feed navbar goes here</Text>
+          <Text>Feed navbar goes here</Text>
+          <Text>Feed navbar goes here</Text>
+        </VStack>
+      </HStack>
     </div>
   );
 };
