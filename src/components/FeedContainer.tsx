b516dd62b6956215a478fcb1e8777204c3faf762
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2023-06-23 12:14:45 +0100

    feed tablist title and panel layout

--- src/components/FeedContainer.tsx
+++ src/components/FeedContainer.tsx
@@ -1,11 +1,21 @@
 'use client';
 
-import { HStack, Text, VStack } from '@chakra-ui/react';
+import {
+  HStack,
+  Tab,
+  TabIndicator,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Tabs,
+  Text,
+  VStack,
+} from '@chakra-ui/react';
 import PrimaryButton from './PrimaryButton';
 
 const FeedContainer = () => {
   return (
-    <VStack border="1px solid #D0D0D0" width="70rem">
+    <VStack border="1px solid #D0D0D0" width="70rem" gap={10}>
       <HStack gap="24rem" pt="1.38rem">
         <VStack alignItems="flex-start" gap="0.75rem">
           <Text
@@ -24,6 +34,28 @@ const FeedContainer = () => {
 
         <PrimaryButton text="Post a content" link="" width="12rem" height="3.5rem" hasIcon />
       </HStack>
+
+      <Tabs pos="relative" variant="unstyled" width="79%">
+        <TabList gap="18rem" border="1px solid #D0D0D0" pl="1rem" py="0.5rem" borderRadius="0.5rem">
+          <Tab fontWeight="500">For you</Tab>
+          <Tab fontWeight="500">Featured</Tab>
+          <Tab fontWeight="500">Recent</Tab>
+        </TabList>
+
+        <TabIndicator mt="-0.3rem" height="0.3rem" bg="#543EE0" borderRadius="1px" />
+
+        <TabPanels border="1px solid #D0D0D0" borderRadius="0.5rem">
+          <TabPanel>
+            <p>One</p>
+          </TabPanel>
+          <TabPanel>
+            <p>Two</p>
+          </TabPanel>
+          <TabPanel>
+            <p>Three</p>
+          </TabPanel>
+        </TabPanels>
+      </Tabs>
     </VStack>
   );
 };
