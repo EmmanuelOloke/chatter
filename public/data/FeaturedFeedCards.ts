3afb4cd419122431ecbdcb41a3b7fd7b10cb81f9
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2023-06-24 21:38:55 +0100

    created FeaturedFeedCards data file [will be a DB request in the future]

---
+++ public/data/FeaturedFeedCards.ts
@@ -0,0 +1,47 @@
+export const FeaturedFeedCards = [
+  {
+    id: 1,
+    authorName: 'Grace Ikpang',
+    authorAvatar: './assets/avatars/grace-ikpang.jpeg',
+    authorJob: 'Product Designer',
+    datePublished: 'May 25th, 2023',
+    articleTitle: 'Starting out as a Product Designer',
+    articleDuration: '10 min read',
+    articleExcerpt:
+      'Embarking on a journey as a product designer can be an exhilarating and fulfilling experience. As a profession that bridges the realms of art, technology, and problem-solving, product design offers an opportunity to shape the way people interact with the world around them.',
+    articleImage: './assets/article-images/product-designer.png',
+    articleComments: 200,
+    articleLikes: 120,
+    articleViews: 2980,
+  },
+  {
+    id: 2,
+    authorName: 'Oluwatobi Oguniyi',
+    authorAvatar: './assets/avatars/oluwatobi-oguniyi.jpeg',
+    authorJob: 'Software Developer',
+    datePublished: 'May 22nd, 2023',
+    articleTitle: 'Using CSS Selectors',
+    articleDuration: '10 min read',
+    articleExcerpt:
+      'Cascading Style Sheets (CSS) is a fundamental technology that lies at the heart of modern web design. It empowers developers and designers to transform plain HTML into visually captivating and interactive web pages',
+    articleImage: './assets/article-images/using-css-selectors.png',
+    articleComments: 200,
+    articleLikes: 120,
+    articleViews: 2980,
+  },
+  {
+    id: 3,
+    authorName: 'Adebobola Muhydeen',
+    authorAvatar: './assets/avatars/adebobola-muhydeen.jpeg',
+    authorJob: 'Political Analyst',
+    datePublished: 'May 23rd, 2023',
+    articleTitle: 'The impact of social media on Political discourse',
+    articleDuration: '10 min read',
+    articleExcerpt:
+      'In the era of digital communication, social media platforms have become pervasive in our daily lives, revolutionizing the way we connect, share information, and engage with the world',
+    articleImage: './assets/article-images/social-media-politics.png',
+    articleComments: 200,
+    articleLikes: 120,
+    articleViews: 2980,
+  },
+];
