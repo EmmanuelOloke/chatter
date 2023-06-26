b254b0b29b6ae7c6101fcdf1e5deabf6ec699c22
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2023-06-26 23:11:48 +0100

    sidebar still not sticky [looking ways around chakra ui]

--- src/app/feed/page.tsx
+++ src/app/feed/page.tsx
@@ -3,17 +3,15 @@
 import FeedContainer from '@/components/FeedContainer';
 import FeedNavBar from '@/components/FeedNavBar';
 import SideBar from '@/components/SideBar';
-import { Box, HStack, VStack } from '@chakra-ui/react';
+import { Box, HStack, Text, VStack } from '@chakra-ui/react';
 import React from 'react';
 
 const Feed = () => {
   return (
     <div>
-      <HStack h="100%">
+      <HStack>
         <HStack alignItems="flex-start" gap={0}>
-          {/* <Box pos="sticky" top="0"> */}
           <SideBar />
-          {/* </Box> */}
 
           <VStack w="100vw" alignItems="flex-start">
             <FeedNavBar />
