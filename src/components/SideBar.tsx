8cd069b55bc4e7304f36b02e1e355ee6ccf6eac8
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2023-06-28 23:42:56 +0100

    finally fixed sticky positioning

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { Flex, Icon, Text, VStack } from '@chakra-ui/react';
+import { Box, Flex, Icon, Text, VStack } from '@chakra-ui/react';
 import NextLink from 'next/link';
 
 import FeedSvg from '@/components/SvgComponents/sidebar-svgs/FeedSvg';
@@ -15,147 +15,140 @@ import { CSSObject } from '@emotion/react';
 
 const SideBar = () => {
   return (
-    <Flex
-      position="sticky"
-      top="0"
-      flexDir="column"
-      w="16rem"
-      h="100vh"
-      borderRight="1px solid #D0D0D0"
-      pt={3}
-      gap={5}
-    >
-      <Flex color="#543EE0" fontSize="1.7rem" justifyContent="center">
-        CHATTER
-      </Flex>
-
-      <Flex justifyContent="center">
-        <Flex fontWeight="400" fontSize="1rem" w="8.5rem" pl={2}>
-          Overview
+    <Box position="sticky" top="0" pt="1rem" w="16rem" h="100vh" borderRight="1px solid #D0D0D0">
+      <Flex flexDir="column" gap={5}>
+        <Flex color="#543EE0" fontSize="1.7rem" justifyContent="center">
+          CHATTER
         </Flex>
-      </Flex>
 
-      <Flex justifyContent="flex-end">
-        <VStack w="9.5rem" alignItems="left">
-          <NextLink href="/feed" passHref>
-            <Flex justifyContent="left" alignItems="center" gap={3}>
-              <Icon viewBox="0 0 22 22">
-                <FeedSvg color="#543EE0" />
-              </Icon>
-              <Text color="#543EE0">Feed</Text>
-            </Flex>
-          </NextLink>
+        <Flex justifyContent="center">
+          <Flex fontWeight="400" fontSize="1rem" w="8.5rem" pl={2}>
+            Overview
+          </Flex>
+        </Flex>
 
-          <NextLink href="/feed" passHref>
-            <Flex justifyContent="left" alignItems="center" gap={3}>
-              <Icon viewBox="0 0 22 22" color="red.500">
-                <BookmarkSvg color="#626262" />
-              </Icon>
-              <Text color="#626262">Bookmarks</Text>
-            </Flex>
-          </NextLink>
+        <Flex justifyContent="flex-end">
+          <VStack w="9.5rem" alignItems="left">
+            <NextLink href="/feed" passHref>
+              <Flex justifyContent="left" alignItems="center" gap={3}>
+                <Icon viewBox="0 0 22 22">
+                  <FeedSvg color="#543EE0" />
+                </Icon>
+                <Text color="#543EE0">Feed</Text>
+              </Flex>
+            </NextLink>
 
-          <NextLink href="/feed" passHref>
-            <Flex justifyContent="left" alignItems="center" gap={3}>
-              <Icon viewBox="0 0 22 22" color="red.500">
-                <TeamSvg color="#626262" />
-              </Icon>
-              <Text color="#626262">Team Blogs</Text>
-            </Flex>
-          </NextLink>
+            <NextLink href="/feed" passHref>
+              <Flex justifyContent="left" alignItems="center" gap={3}>
+                <Icon viewBox="0 0 22 22" color="red.500">
+                  <BookmarkSvg color="#626262" />
+                </Icon>
+                <Text color="#626262">Bookmarks</Text>
+              </Flex>
+            </NextLink>
 
-          <NextLink href="/feed" passHref>
-            <Flex justifyContent="left" alignItems="center" gap={3}>
-              <Icon viewBox="0 0 22 22" color="red.500">
-                <DraftsSvg color="#626262" />
-              </Icon>
-              <Text color="#626262">Drafts</Text>
-            </Flex>
-          </NextLink>
+            <NextLink href="/feed" passHref>
+              <Flex justifyContent="left" alignItems="center" gap={3}>
+                <Icon viewBox="0 0 22 22" color="red.500">
+                  <TeamSvg color="#626262" />
+                </Icon>
+                <Text color="#626262">Team Blogs</Text>
+              </Flex>
+            </NextLink>
 
-          <NextLink href="/feed" passHref>
-            <Flex justifyContent="left" alignItems="center" gap={3}>
-              <Icon viewBox="0 0 22 22" color="red.500">
-                <AnalyticsSvg color="#626262" />
-              </Icon>
-              <Text color="#626262">Analytics</Text>
-            </Flex>
-          </NextLink>
-        </VStack>
-      </Flex>
+            <NextLink href="/feed" passHref>
+              <Flex justifyContent="left" alignItems="center" gap={3}>
+                <Icon viewBox="0 0 22 22" color="red.500">
+                  <DraftsSvg color="#626262" />
+                </Icon>
+                <Text color="#626262">Drafts</Text>
+              </Flex>
+            </NextLink>
 
-      <Flex justifyContent="center">
-        <Flex w="8.5rem" alignItems="center" gap={3} ml={2}>
-          <Text fontWeight="400" fontSize="1rem">
-            Trending Tags
+            <NextLink href="/feed" passHref>
+              <Flex justifyContent="left" alignItems="center" gap={3}>
+                <Icon viewBox="0 0 22 22" color="red.500">
+                  <AnalyticsSvg color="#626262" />
+                </Icon>
+                <Text color="#626262">Analytics</Text>
+              </Flex>
+            </NextLink>
+          </VStack>
+        </Flex>
+
+        <Flex justifyContent="center">
+          <Flex w="8.5rem" alignItems="center" gap={3} ml={2}>
+            <Text fontWeight="400" fontSize="1rem">
+              Trending Tags
+            </Text>
+
+            <Icon viewBox="0 0 22 22" color="red.500" mt={2}>
+              <TrendingSvg color="#626262" />
+            </Icon>
+          </Flex>
+        </Flex>
+
+        <Flex justifyContent="flex-end">
+          <VStack w="9.5rem" alignItems="left">
+            <NextLink href="/feed" passHref>
+              <Text color="#626262">Programming</Text>
+            </NextLink>
+
+            <NextLink href="/feed" passHref>
+              <Text color="#626262">Data Science</Text>
+            </NextLink>
+
+            <NextLink href="/feed" passHref>
+              <Text color="#626262">Technology</Text>
+            </NextLink>
+
+            <NextLink href="/feed" passHref>
+              <Text color="#626262">Machine Learning</Text>
+            </NextLink>
+
+            <NextLink href="/feed" passHref>
+              <Text color="#626262">Politics</Text>
+            </NextLink>
+          </VStack>
+        </Flex>
+
+        <Flex justifyContent="center">
+          <Flex w="8.5rem" alignItems="center" gap={3} ml={2}>
+            <Text fontWeight="400" fontSize="1rem">
+              Personal
+            </Text>
+          </Flex>
+        </Flex>
+
+        <Flex justifyContent="flex-end">
+          <VStack w="9.5rem" alignItems="left">
+            <NextLink href="/feed" passHref>
+              <Flex justifyContent="left" alignItems="center" gap={3}>
+                <Icon viewBox="0 0 22 22">
+                  <AccountSvg color="#626262" />
+                </Icon>
+                <Text color="#626262">Account</Text>
+              </Flex>
+            </NextLink>
+
+            <NextLink href="/feed" passHref>
+              <Flex justifyContent="left" alignItems="center" gap={3}>
+                <Icon viewBox="0 0 22 22" color="red.500">
+                  <NotificationsSvg color="#626262" />
+                </Icon>
+                <Text color="#626262">Notifications</Text>
+              </Flex>
+            </NextLink>
+          </VStack>
+        </Flex>
+
+        <NextLink href="/logout" passHref>
+          <Text color="#FF1400" textAlign="center">
+            Log Out
           </Text>
-
-          <Icon viewBox="0 0 22 22" color="red.500" mt={2}>
-            <TrendingSvg color="#626262" />
-          </Icon>
-        </Flex>
+        </NextLink>
       </Flex>
-
-      <Flex justifyContent="flex-end">
-        <VStack w="9.5rem" alignItems="left">
-          <NextLink href="/feed" passHref>
-            <Text color="#626262">Programming</Text>
-          </NextLink>
-
-          <NextLink href="/feed" passHref>
-            <Text color="#626262">Data Science</Text>
-          </NextLink>
-
-          <NextLink href="/feed" passHref>
-            <Text color="#626262">Technology</Text>
-          </NextLink>
-
-          <NextLink href="/feed" passHref>
-            <Text color="#626262">Machine Learning</Text>
-          </NextLink>
-
-          <NextLink href="/feed" passHref>
-            <Text color="#626262">Politics</Text>
-          </NextLink>
-        </VStack>
-      </Flex>
-
-      <Flex justifyContent="center">
-        <Flex w="8.5rem" alignItems="center" gap={3} ml={2}>
-          <Text fontWeight="400" fontSize="1rem">
-            Personal
-          </Text>
-        </Flex>
-      </Flex>
-
-      <Flex justifyContent="flex-end">
-        <VStack w="9.5rem" alignItems="left">
-          <NextLink href="/feed" passHref>
-            <Flex justifyContent="left" alignItems="center" gap={3}>
-              <Icon viewBox="0 0 22 22">
-                <AccountSvg color="#626262" />
-              </Icon>
-              <Text color="#626262">Account</Text>
-            </Flex>
-          </NextLink>
-
-          <NextLink href="/feed" passHref>
-            <Flex justifyContent="left" alignItems="center" gap={3}>
-              <Icon viewBox="0 0 22 22" color="red.500">
-                <NotificationsSvg color="#626262" />
-              </Icon>
-              <Text color="#626262">Notifications</Text>
-            </Flex>
-          </NextLink>
-        </VStack>
-      </Flex>
-
-      <NextLink href="/logout" passHref>
-        <Text color="#FF1400" textAlign="center">
-          Log Out
-        </Text>
-      </NextLink>
-    </Flex>
+    </Box>
   );
 };
 
