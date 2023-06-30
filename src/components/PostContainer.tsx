8447b753c728d069e1b907749f09c2ac2adeceb4
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2023-06-30 12:40:26 +0100

    created Textarea for the markdown post

--- src/components/PostContainer.tsx
+++ src/components/PostContainer.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { Button, HStack, VStack } from '@chakra-ui/react';
+import { Button, HStack, Textarea, VStack } from '@chakra-ui/react';
 import PrimaryButton from './PrimaryButton';
 
 const PostContainer = () => {
@@ -8,7 +8,7 @@ const PostContainer = () => {
     <VStack
       border="1px solid #D0D0D0"
       w="70rem"
-      h="40rem"
+      h="full"
       borderRadius=".5rem"
       mt="1.87rem"
       ml="1.5rem"
@@ -16,6 +16,23 @@ const PostContainer = () => {
       <HStack w="50rem" justifyContent="flex-end" mt="3rem">
         <PrimaryButton text="Publish" link="" width="12rem" height="3.5rem" disabled />
       </HStack>
+
+      <Textarea
+        resize="vertical"
+        overflowY="auto"
+        minHeight="37rem"
+        mx="3rem"
+        my="2rem"
+        w="67rem"
+        border="none"
+      >
+        The editor will go here The editor will go here The editor will go here The editor will go
+        here The editor will go here The editor will go here The editor will go here The editor will
+        go here The editor will go here The editor will go here The editor will go here The editor
+        will go here The editor will go here The editor will go here The editor will go here The
+        editor will go here The editor will go here The editor will go here The editor will go here
+        The editor will go here The editor will go here
+      </Textarea>
     </VStack>
   );
 };
