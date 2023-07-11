744fc23958e81fa2224e69a3c3aa4105945b691a
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2023-07-11 14:36:43 +0100

    feat: input field optimization in progress

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,14 +1,9 @@
 // import { FormControl, FormErrorMessage, FormLabel, Input } from '@chakra-ui/react';
-// import { Field, FieldHookConfig, useField } from 'formik';
+// import { Field, useField } from 'formik';
 // import React from 'react';
 
-// type FieldProps = {
-//   label: string;
-//   props: FieldHookConfig<any>;
-// };
-
-// const InputField = ({ label, ...props }: FieldProps) => {
-//   const [field, meta] = useField(props.name);
+// const InputField = ({ label, ...props }) => {
+//   const [field, meta] = useField(props);
 //   return (
 //     <FormControl id="firstName" isInvalid={Boolean(meta.error && meta.touched)}>
 //       <FormLabel>{label}</FormLabel>
