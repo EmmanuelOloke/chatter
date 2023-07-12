f6c940f435658717d1960fa56085cb075c3f3655
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2023-07-12 12:14:21 +0100

    add: refactored mongodb connection

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,19 +1,13 @@
-import mongoose from 'mongoose';
+import mongoose, { mongo } from 'mongoose';
 
 const { MONGODB_URI } = process.env;
+let connection: typeof mongoose;
 
 if (!MONGODB_URI) {
   throw new Error('Invalid environment variable: MONGODB_URI');
 }
 
 export const connectToMongoDB = async () => {
-  try {
-    const { connection } = await mongoose.connect(MONGODB_URI);
-
-    if (connection.readyState === 1) {
-      return Promise.resolve(true);
-    }
-  } catch (error) {
-    return Promise.reject(error);
-  }
+  if (!connection) connection = await mongoose.connect(MONGODB_URI);
+  return connection;
 };
