8f4e7a3fd352a02eabfdbe1ebb18ca47db2edc88
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2023-07-18 00:12:12 +0100

    feat: moved feed page into a private route

---
+++ src/app/(private_route)/feed/page.tsx
@@ -0,0 +1,27 @@
+'use client';
+
+import FeedContainer from '@/components/FeedContainer';
+import FeedNavBar from '@/components/FeedNavBar';
+import SideBar from '@/components/SideBar';
+import { Box, HStack, VStack } from '@chakra-ui/react';
+import React from 'react';
+
+const Feed = () => {
+  return (
+    <HStack>
+      <HStack height="100vh" alignItems="flex-start" gap={0} overflowY="auto">
+        <SideBar />
+
+        <VStack w="100vw" alignItems="flex-start">
+          <FeedNavBar />
+
+          <Box ml="1rem" mt="1rem">
+            <FeedContainer />
+          </Box>
+        </VStack>
+      </HStack>
+    </HStack>
+  );
+};
+
+export default Feed;
