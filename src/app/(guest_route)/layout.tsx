a09e7a1a806d50dabde5b1e441be8573efd26f46
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2023-07-18 09:49:49 +0100

    feat: created guest_route layout, prevents logged in users from accessing guest routes

---
+++ src/app/(guest_route)/layout.tsx
@@ -0,0 +1,18 @@
+import React, { ReactNode } from 'react';
+import { getServerSession } from 'next-auth';
+import { authOptions } from './api/auth/[...nextauth]/route';
+import { redirect } from 'next/navigation';
+
+interface Props {
+  children: ReactNode;
+}
+
+export default async function GuestLayout({ children }: Props) {
+  const session = await getServerSession(authOptions);
+
+  if (session?.user) redirect('/feed');
+
+  console.log(session?.user);
+
+  return <>{children}</>;
+}
