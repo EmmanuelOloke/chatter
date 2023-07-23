d1681f5eb18ac0fb38fc92a30d77823f514b23bb
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2023-07-23 22:32:31 +0100

    feat: export Buttons for GoogleSignUp and LinkedInSignUp

--- src/components/AuthButtons.tsx
+++ src/components/AuthButtons.tsx
@@ -3,6 +3,45 @@
 import { Button, Image } from '@chakra-ui/react';
 import { signIn } from 'next-auth/react';
 import { FcGoogle } from 'react-icons/fc';
+import NextLink from 'next/link';
+
+export const GoogleSignUpButton = () => {
+  return (
+    <NextLink href="/api/auth/google">
+      <Button
+        leftIcon={<FcGoogle />}
+        variant="outline"
+        size="lg"
+        width="100%"
+        height="3rem"
+        fontWeight="bold"
+        backgroundColor="#e9effe"
+      >
+        Sign up with Google
+      </Button>
+    </NextLink>
+  );
+};
+
+export const LinkedInSignUpButton = () => {
+  return (
+    <NextLink href="/api/linkedin">
+      <Button
+        leftIcon={
+          <Image src={'./assets/icons/linkedin-icon.png'} alt="linkedin icon" width="1.2rem" />
+        }
+        variant="outline"
+        size="lg"
+        width="100%"
+        height="3rem"
+        fontWeight="bold"
+        backgroundColor="#e9effe"
+      >
+        Sign up with LinkedIn
+      </Button>
+    </NextLink>
+  );
+};
 
 export const GoogleSignInButton = () => {
   const handleClick = () => {
