8c6cbe77fb289708bbcabe8bd0609d850ad4be04
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2023-07-26 22:52:30 +0100

    feat: rendered ProfileUpdateForm

--- src/components/AccountContainer.tsx
+++ src/components/AccountContainer.tsx
@@ -1,31 +1,12 @@
 'use client';
 
-import {
-  Avatar,
-  HStack,
-  Image,
-  Tab,
-  TabIndicator,
-  TabList,
-  TabPanel,
-  TabPanels,
-  Tabs,
-  Text,
-  VStack,
-} from '@chakra-ui/react';
-import PrimaryButton from './PrimaryButton';
-import { ForYouFeedCards } from '../../public/data/ForYouFeedCards';
-import { FeaturedFeedCards } from '../../public/data/FeaturedFeedCards';
-import ReadSvg from './SvgComponents/feed-cards-svgs/ReadSvg';
-import CommentSvg from './SvgComponents/feed-cards-svgs/CommentSvg';
-import LikeSvg from './SvgComponents/feed-cards-svgs/LikeSvg';
-import AnalyticsSvg from './SvgComponents/sidebar-svgs/AnalyticsSvg';
-import { RecentFeedCards } from '../../public/data/RecentFeedCards';
+import { Box, HStack, Text, VStack } from '@chakra-ui/react';
+import ProfileUpdateForm from './Forms/ProfileUpdateForm';
 
 const AccountContainer = () => {
   return (
     <VStack border="1px solid #D0D0D0" width="70rem" gap={10} pb="3rem" mb="3rem">
-      <HStack w="79%">
+      <HStack w="70%">
         <VStack gap="0.75rem" alignItems="flex-start">
           <Text fontSize="2rem" fontWeight="500" lineHeight="3rem" color="#111">
             Account
@@ -35,6 +16,10 @@ const AccountContainer = () => {
           </Text>
         </VStack>
       </HStack>
+
+      <Box w="70%">
+        <ProfileUpdateForm />
+      </Box>
     </VStack>
   );
 };
