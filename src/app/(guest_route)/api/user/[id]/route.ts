4b05131aa6a4bc5ed7e1b2b4b08c23331f683cbe
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2023-07-31 23:16:47 +0100

    feat: PUT api endpoint to allow user update their details

--- src/app/(guest_route)/api/user/[id]/route.ts
+++ src/app/(guest_route)/api/user/[id]/route.ts
@@ -1,6 +1,7 @@
 import User from '../../../../../../models/user';
 import { connectToMongoDB } from '../../../../../../lib/mongodb';
 import { NextRequest, NextResponse } from 'next/server';
+import bycrypt from 'bcrypt';
 
 export async function GET(request: NextRequest, context: { params: { id: string } }) {
   await connectToMongoDB();
@@ -13,4 +14,30 @@ export async function GET(request: NextRequest, context: { params: { id: string 
   const user = await User.findById(id);
 
   return NextResponse.json(user);
+}
+
+export async function PUT(request: NextRequest, context: { params: { id: string } }) {
+  try {
+    const body = await request.json();
+    const { firstName, lastName, email, role, password } = body;
+
+    await connectToMongoDB();
+
+    const userId = context.params.id;
+
+    const salt = await bycrypt.genSalt(12);
+    const hashedPassword = await bycrypt.hash(password, salt);
+
+    const filter = { _id: userId };
+    const update = { $set: { firstName, lastName, email, role, password: hashedPassword } };
+
+    const updated = await User.updateOne(filter, update);
+
+    return NextResponse.json(
+      { message: 'User details updated successfully', updated },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.log('Error updating your profile', error);
+  }
 }
