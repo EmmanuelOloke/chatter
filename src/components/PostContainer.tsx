8e4b3b734bb7832061d1f96fde842005587135ce
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2023-08-02 16:51:41 +0100

    feat: use ImageUploadButton and CopyButton components with the useCopy hook for the copy to clipboard functionality

--- src/components/PostContainer.tsx
+++ src/components/PostContainer.tsx
@@ -1,11 +1,15 @@
 'use client';
 
-import { Box, HStack, VStack } from '@chakra-ui/react';
+import { Box, Button, HStack, Input, VStack } from '@chakra-ui/react';
 import '@uiw/react-md-editor/markdown-editor.css';
 import '@uiw/react-markdown-preview/markdown.css';
 import dynamic from 'next/dynamic';
 import PrimaryButton from './PrimaryButton';
 import { useState } from 'react';
+import ImageUploadButton from './Buttons/ImageUploadButton';
+import { CopyIcon } from '@chakra-ui/icons';
+import { CopyButton } from './Buttons/CopyButton';
+import useCopy from '../../lib/custom-hooks/useCopy';
 
 const MDEditor = dynamic(() => import('@uiw/react-md-editor'), { ssr: false });
 
@@ -15,6 +19,12 @@ const MDEditor = dynamic(() => import('@uiw/react-md-editor'), { ssr: false });
 
 const PostContainer = () => {
   const [value, setValue] = useState<string | undefined>('');
+  const [postImageUrl, setPostImageUrl] = useState<string>('');
+  const postImageMarkdownFormat = `![Image description](${postImageUrl})`;
+
+  const { copyFormattedMdImageLink, isCopied } = useCopy();
+
+  console.log(postImageUrl);
 
   return (
     <VStack
@@ -25,9 +35,21 @@ const PostContainer = () => {
       mt=".5rem"
       ml="1.5rem"
     >
-      <HStack w="60rem" justifyContent="flex-end" mt="3rem">
-        <PrimaryButton text="Publish" link="" width="12rem" height="3.5rem" disabled={!value} />
-      </HStack>
+      <VStack alignItems="flex-start">
+        <HStack w="60rem" justifyContent="space-between" alignItems="center" mt="3rem">
+          <ImageUploadButton setImageUrl={setPostImageUrl} />
+          <PrimaryButton text="Publish" link="" width="12rem" height="3.5rem" disabled={!value} />
+        </HStack>
+
+        <HStack>
+          <Input type="text" value={postImageMarkdownFormat} w="30rem" />
+          <CopyButton
+            handleClick={() => copyFormattedMdImageLink(postImageMarkdownFormat)}
+            text={isCopied ? 'Copied!' : 'Copy...'}
+            icon={<CopyIcon />}
+          />
+        </HStack>
+      </VStack>
 
       <Box
         overflowY="auto"
