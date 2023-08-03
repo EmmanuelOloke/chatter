1a0c20f493c2e206f99310879a451dde80a6d887
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2023-08-03 23:10:09 +0100

    feat: create initial Post Schema

---
+++ models/post.ts
@@ -0,0 +1,79 @@
+import { Model, Schema, model, models } from 'mongoose';
+import { Document } from 'mongoose';
+
+interface PostDocument extends Document {
+  title: string;
+  content: string;
+  author: string;
+  datePosted: Date;
+  comments: {}[];
+  likes: number;
+  views: number;
+  coverImage: string;
+  authorImage: string;
+  authorPosition: string;
+  timeToRead: string;
+}
+
+const PostSchema = new Schema<PostDocument>({
+  title: {
+    type: String,
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  author: {
+    type: String,
+    required: true,
+  },
+  datePosted: {
+    type: Date,
+    default: Date.now(),
+  },
+  comments: [
+    {
+      user: {
+        type: String,
+        required: true,
+      },
+      comment: {
+        type: String,
+        required: true,
+      },
+      datePosted: {
+        type: Date,
+        default: Date.now(),
+      },
+    },
+  ],
+  likes: {
+    type: Number,
+    default: 0,
+  },
+  views: {
+    type: Number,
+    default: 0,
+  },
+  coverImage: {
+    type: String,
+    required: true,
+  },
+  authorImage: {
+    type: String,
+    required: true,
+  },
+  authorPosition: {
+    type: String,
+    required: true,
+  },
+  timeToRead: {
+    type: String,
+    required: true,
+  },
+});
+
+const Post = models.Post || model('Post', PostSchema);
+
+export default Post as Model<PostDocument>;
