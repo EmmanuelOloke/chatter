446187f7d0f3ae3ca4b9540cf026beeaa22d0410
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2023-08-10 13:09:54 +0100

    feat: add profession field validation rules

--- lib/yup-validation-schemas/profile-update-form.ts
+++ lib/yup-validation-schemas/profile-update-form.ts
@@ -4,6 +4,7 @@ export const profileUpdateValidationSchema = Yup.object({
   firstName: Yup.string().required('First name is required').min(2, 'First name is too short'),
   lastName: Yup.string().required('Last name is required').min(2, 'Last name is too short'),
   email: Yup.string().required('Email is required').email('Invalid email address'),
+  profession: Yup.string().required('Profession is required').min(2, 'Profession is too short'),
   password: Yup.string()
     .required('Password is required')
     .min(8, 'Password must be at least 8 characters'),
